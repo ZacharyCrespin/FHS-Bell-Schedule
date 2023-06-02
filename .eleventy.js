de4b1d2efe722931eab59e1db7c32c1d06688452
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-02 19:09:34 +0000

    api

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/manifest.json")
   eleventyConfig.addPassthroughCopy("src/robots.txt")
   
+  eleventyConfig.addFilter('stringify', function (obj) {
+    return JSON.stringify(obj);
+  });
+
   return {
     dir: {
       input: "src",
