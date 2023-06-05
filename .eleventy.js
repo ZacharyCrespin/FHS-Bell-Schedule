1d06dd542956f3cd7242ccc6fb14c22bacee3f83
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-05 09:54:57 -0700

    add google verification file

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/icons")
   eleventyConfig.addPassthroughCopy("src/_redirects")
   eleventyConfig.addPassthroughCopy("src/favicon.ico")
+  eleventyConfig.addPassthroughCopy("src/google9280d1828cb7dc65.html")
   eleventyConfig.addPassthroughCopy("src/main.css")
   eleventyConfig.addPassthroughCopy("src/manifest.json")
   eleventyConfig.addPassthroughCopy("src/robots.txt")
