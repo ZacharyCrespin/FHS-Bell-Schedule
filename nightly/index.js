460b6d5c2bb18972ef44fa4d19c4daf6980845af
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-10 13:59:29 -0700

    remove 'nightly'

--- nightly/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require('@google-cloud/functions-framework');
-
-const buildHook = "https://api.netlify.com/build_hooks/6480ffb5b3dfa84c24f552a4"
-
-functions.http('nightly', (req, res) => {
-  fetch(buildHook, {method: 'POST'});
-});
