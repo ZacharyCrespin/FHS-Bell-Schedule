512f7f809d2b75a070649e60374983fe5deb1212
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-13 16:43:18 -0700

    fix passthroughs

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,14 @@ const filesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(filesMinifier);
 
+  eleventyConfig.addPassthroughCopy("src/fonts")
   eleventyConfig.addPassthroughCopy("src/icons")
   eleventyConfig.addPassthroughCopy("src/_redirects")
   eleventyConfig.addPassthroughCopy("src/favicon.ico")
   eleventyConfig.addPassthroughCopy("src/google9280d1828cb7dc65.html")
   eleventyConfig.addPassthroughCopy("src/manifest.json")
   eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml")
 
   eleventyConfig.addFilter('stringify', function (obj) {
     return JSON.stringify(obj);
