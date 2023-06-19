0fc5da134be8e76970f02fa363e603c912d158ac
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-18 20:01:47 -0700

    more converting to webc

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,22 @@
 // Settings
-const savedSettings = JSON.parse(localStorage.getItem("fhsbs-settings"))
-if (savedSettings) {
-  document.body.classList = `theme-${savedSettings.theme}`
-  if (savedSettings.theme === "custom") {
-    const background = savedSettings.customTheme.background
-    const foreground = savedSettings.customTheme.foreground
-    const radius = savedSettings.customTheme.radius
-    if (background) {
-      document.documentElement.style.setProperty('--background', background)
-    }
-    if (foreground) {
-      document.documentElement.style.setProperty('--foreground', foreground)
-    }
-    if (radius) {
-      document.documentElement.style.setProperty('--radius', radius)
+async function applySettings() {
+  const savedSettings = JSON.parse(localStorage.getItem("fhsbs-settings"))
+  if (savedSettings) {
+    document.body.classList = `theme-${savedSettings.theme}`
+    if (savedSettings.theme === "custom") {
+      const background = savedSettings.customTheme.background
+      const foreground = savedSettings.customTheme.foreground
+      const radius = savedSettings.customTheme.radius
+      if (background) {
+        document.documentElement.style.setProperty('--background', background)
+      }
+      if (foreground) {
+        document.documentElement.style.setProperty('--foreground', foreground)
+      }
+      if (radius) {
+        document.documentElement.style.setProperty('--radius', radius)
+      }
     }
   }
 }
-
-// Menu
-const menuIcon = document.getElementById("menu-icon")
-const menu = document.getElementById("menu")
-
-menuIcon.addEventListener("click", () => {
-  menu.classList.toggle("open")
-})
+applySettings()
