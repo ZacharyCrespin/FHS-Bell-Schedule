30e6ea33d6a60309ad7024acb1d6a8beb1578ef4
Zachary Crespin <Zachary.lights@gmail.com>
2023-09-01 17:29:45 +0000

    add tomorrow page

--- src/_data/tomorrow.js
+++ src/_data/tomorrow.js
@@ -1,15 +1,15 @@
 const { DateTime } = require('luxon')
 const { getDate, getSchedule, getEvents, getGames } = require('../../get')
 
-const dateStr = DateTime.now().setZone('America/Los_Angeles').toFormat('MM/dd/yyyy')
+const dateStr = DateTime.now().setZone('America/Los_Angeles').plus({ days: 1 }).toFormat('MM/dd/yyyy')
 
 module.exports = async function() {
   const date = await getDate(dateStr)
 
   const schedule = await getSchedule(dateStr)
 
-  const events = await getEvents(dateStr)
-  const games = await getGames(dateStr)
+  const events = await getEvents(dateStr, false)
+  const games = await getGames(dateStr, false)
 
   return {
     date,
